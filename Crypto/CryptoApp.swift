c319cfc51a8b4b4d85553973e499209c5ac52f81
Loren <liangliangpro@outlook.com>
2023-12-27 22:34:42 +0800

    feat: init

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by loren on 2023/12/27.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
